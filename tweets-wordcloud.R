d061fb458e32d2070f433bed7109b98321250e59
Matteo Redaelli <matteo.redaelli@libero.it>
2011-08-09 22:46:56 +0200

    updated licenses

--- tweets-wordcloud.R
+++ tweets-wordcloud.R
@@ -1,3 +1,16 @@
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ## Wordcloud of tweets
 ##
 ## By Matteo DOT Redaelli AT gmail DOT com
@@ -6,28 +19,6 @@
 ##
 ## Ideas adapted from
 ##    http://onertipaday.blogspot.com/2011/07/word-cloud-in-r.html
-
-##                   GNU GENERAL PUBLIC LICENSE
-##                       Version 3, 29 June 2007
-##
-## Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
-## Everyone is permitted to copy and distribute verbatim copies
-## of this license document, but changing it is not allowed.
-##                            Preamble
-##
-##  The GNU General Public License is a free, copyleft license for
-##software and other kinds of works.
-##
-##  The licenses for most software and other practical works are designed
-##to take away your freedom to share and change the works.  By contrast,
-##the GNU General Public License is intended to guarantee your freedom to
-##share and change all versions of a program--to make sure it remains free
-##software for all its users.  We, the Free Software Foundation, use the
-##GNU General Public License for most of our software; it applies also to
-##any other work released this way by its authors.  You can apply it to
-##your programs, too.
-##
-## see http://www.gnu.org/licenses/gpl-3.0.txt
 
 library(tm)
 library(methods)
